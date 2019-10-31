5d14d9b05bb3aefed695619ec240acbfa407f19f
Ta Hoang Nhat <hoangnhat@jv-it.com.vn>
2019-10-31 16:53:36 +0700

    build struct app

---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="login-form">
+        Login fom
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    props: {
+        msg: String
+    }
+}
+</script>
